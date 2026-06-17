4ae008497abb52e17e6cd6277a30cfb23a632867
temurakhmerov-cmyk <temur.akhmerov@gmail.com>
2026-06-18 00:02:59 +0300

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,10 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    sys.path.append(str(Path(__file__).resolve().parent))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinema_project.settings')
     try:
         from django.core.management import execute_from_command_line
