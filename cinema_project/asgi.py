5a447d9a172b237b9c9d83a571ce30236fa070db
temurakhmerov-cmyk <temur.akhmerov@gmail.com>
2026-06-18 00:08:35 +0300

    Add files via upload

---
+++ cinema_project/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for cinema_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinema_project.settings')
+
+application = get_asgi_application()
